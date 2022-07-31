68e2a39acc2cbb495200f58d134b8c825c7d709f
MANISH HOLLA <29548788+manishholla@users.noreply.github.com>
2022-08-01 01:54:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
 
           <a href="https://drinkable.in/partner" className={styles.card}>
             <h2>Partner with Us &rarr;</h2>
-            <p><text>Already know about us! Let&aposs be Partners.</p>
+            <p><text>Already know about us! Let&aposs be Partners.</text> </p>
           </a>
 
           <a
