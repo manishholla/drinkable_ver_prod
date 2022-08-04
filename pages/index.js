3f45a7e18881b07f78834cb4162c83f3256756dd
MANISH HOLLA <29548788+manishholla@users.noreply.github.com>
2022-08-04 23:35:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://drinkable.in"
           target="_blank"
           rel="noopener noreferrer"
         >
